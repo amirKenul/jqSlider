2a232cdb08d13093c540c0a1c78679deb7c73e5e
Amirmetova Kenul <konul.e@code.edu.az>
2016-08-11 01:33:51 +0400

    it's not work

--- initializr/js/main.js
+++ initializr/js/main.js
@@ -1,22 +1,7 @@
 
 
-/*$(document).ready(function(){
-	$("li")
-	.on("click",function(){
-		$(this).hasClass("active");
-		var a=$(this).addClass("active");
+/*
 
-	})
-	if (a == true){
-		.on("click",function(){
-			$(this).removeClass("bg");
-		})
-	}
-
-
-})
-
-*/
 $(document).ready(function(){
 	$('li')
 	.on('click',function(){
@@ -34,4 +19,22 @@ $(function(){
 		var panelId = $(this).attr("data-panelid");
 		$("#"+panelId).toggle();
 	});
-});
+});*/
+
+
+var image=["../img/1.jpg","../img/2.jpg","../img/3.jpg","../img/4.jpg","../img/5jpg"];
+
+count=0;
+
+$(function(){
+	$("#upBut").on('click',function(){
+		
+		$(".imageLitle").css("background-image:"+count[0]);
+		count++;
+		if(count==4){
+			count=0;
+
+
+	});
+		}
+})
